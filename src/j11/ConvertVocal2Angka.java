d344f2f646bf37389fcfd3e6f1cbf93a55c824fc
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-08-15 09:25:07 +0700

    Initial Commit

--- src/j11/ConvertVocal2Angka.java
+++ src/j11/ConvertVocal2Angka.java
@@ -15,47 +15,5 @@ public class ConvertVocal2Angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
-    public static void main(String[] args) {
-      
-        String identitas = " Rafter Maulana Hokiananda / X RPL 2 / 27 ";
-        
-        tampilJudul(identitas); 
-    }    
-    private static String tampilInput(){
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Masukkan Kalimat: ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
-        
-        return kalimat;
-    }    
-        String kalimat = tampilInput();
-        
-    private static String vocal2Angka(String kalimat){
-        char [][] arConvert = 
-        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-        
-        kalimat = kalimat.toLowerCase();
-        for(int i=0;i<arConvert.length;i++)
-            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-        
-        return kalimat;
-    }
-    String convert = vocal2Angka(kalimat);
     
-    private static void tampilPerKata(String kalimat, String convert){
-        String[]arrKal = kalimat.split(" ");
-        String[]arrcon = convert.split(" ");
-        
-        for(int i=0;i<arrKal.length;i++)
-            System.out.println(arrKal[i]+" => "+arrcon[i]);
-    }    
-        
-        
-    private static void tampilHasil(String convert){
-           System.out.println("Kalimat Alay Angka : " + convert);
-    
-    tampilHasil(convert);
-    }
 }
