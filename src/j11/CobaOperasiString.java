67dd1a5d03ba63c40636f0181c36162c0672997d
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-08-15 08:57:48 +0700

    Initial Commit

--- src/j11/CobaOperasiString.java
+++ src/j11/CobaOperasiString.java
@@ -15,64 +15,7 @@ public class CobaOperasiString {
         String identitas = "Rafter Maulana Hokiananda / XRPL2 / 27";
         System.out.println("Identitas : " + identitas);
         
-        String x = "Operasi";
-        System.out.println("isi variabel x : "+ x);
-        System.out.println("\"" + x + "\" panjang karakter = "+ x.length());
-        
-        System.out.println("x adalah kosong: " + x.isEmpty());
-        
-        String y = "";
-        System.out.println("isi variabel y : "+ y);
-        System.out.println("y adalah kosong : " + y.isEmpty());
-        
-        System.out.println("isi x sama dengan y : " + x.equals(y));
-        
-        String z ="Operasi";
-        System.out.println("isi variabel z : "+ z);
-        System.out.println("isi x sama dengan z (Case Sensitive)" + x.equals(z));
-        
-        String r ="Operasi";
-        System.out.println("isi variabel z : "+ r);
-        System.out.println("isi x sama dengan r (Case Sensitive)" + x.equals(r));
-        System.out.println("isi x sama dengan r (Not Case Sensitive "+ x.equalsIgnoreCase(r));
-    
-        System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
-        System.out.println("perbandingan isi x dengan z (Case Sensitive)" + x.compareTo(z));
-        System.out.println("perbandingan isi x dengan z (Case Sensitive)" + x.compareTo(r));
-        System.out.println("perbandingan isi x dengan r (Not Case Sensitive) "+ r.compareToIgnoreCase(r));
-        
-        String s = "operasi";
-        System.out.println("isi variabel s : "+ s);
-        System.out.println("perbandingan isi x dengan z (Case Sensitive)" + x.compareTo(s));
-        
-        System.out.println("pada x terdapat huruf 'a' : "+ x.contains("a"));
-        
-        System.out.println("isi variabel x besar semua : " + x.toUpperCase());
-        System.out.println("isi variabel x besar semua : " + x.toUpperCase());
-        
-        String t = "  opearsi string ";
-        System.out.println("isi variabel t : \""+ t +"\"");
-        System.out.println("isi variabel t tanpa spasi : \""+ t.trim());
-        
-        System.out.println("gabungkan isi vaiabel x dan r : "+ x.concat(r));
-        System.out.println("karakter mulai posisi 3 di variabel x : " + x.substring(3));
-        System.out.println("karakter mulai posisi 3-5 di variabel x : " + x.substring(3,6));
-        
-        String tclean = t.trim();
-        System.out.println("isi variabel tclean : "+ t.trim());
-        System.out.println("rubah 'i' jadi 'E' di variabel tclean"+ tclean.replace("i","E"));
-        
-        String[] arrT = tclean.split("r");
-        System.out.println("pecah kata di varaibel tclean :");
-        for (int i = 0; i < arrT.length; i++) 
-            System.out.println(arrT[i]);
-            
-        String[] arrTr = tclean.split("r");
-        System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
-        for (int i=0; i < arrTr.length; i++) 
-            System.out.println(arrTr[i]);
-        
-              
+       
         }
     }
 
